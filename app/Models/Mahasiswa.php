0371d54fe6b2a9720eaabda9e124278c141e48ee
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-27 23:21:39 +0700

    Setting $fillable pada Model Mahasiswa berdasarkan data yang ingin dapat ditambah atau diedit pada kasus ini Nim, Nama, Kelas, Jurusan dan No_Handphone

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
+    protected $table="mahasiswa"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    public $timestamps= false;
+    protected $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable. *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+    ];
+
 }
