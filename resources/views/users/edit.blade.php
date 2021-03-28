13c07fc3d2c7f734f5f63917e02020c535a7bf9c
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-28 10:48:30 +0700

    Tugas 1 - Memodifikasi edit.blade.php agar dapat mengedit Tanggal_Lahir dan Email

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -35,6 +35,11 @@
                                 aria-describedby="Nama">
                         </div>
                         <div class="form-group">
+                            <label for="Tanggal_Lahir">Tanggal_Lahir</label>
+                            <input type="date" name="Tanggal_Lahir" class="form-control" id="Tanggal_Lahir"
+                                value="{{ $Mahasiswa->Tanggal_Lahir }}" aria-describedby="Tanggal_Lahir">
+                        </div>
+                        <div class="form-group">
                             <label for="Kelas">Kelas</label>
                             <input type="Kelas" name="Kelas" class="form-control" id="Kelas"
                                 value="{{ $Mahasiswa->Kelas }}" aria-describedby="Kelas">
@@ -43,6 +48,11 @@
                             <label for="Jurusan">Jurusan</label>
                             <input type="Jurusan" name="Jurusan" class="form-control" id="Jurusan"
                                 value="{{ $Mahasiswa->Jurusan }}" aria-describedby="Jurusan">
+                        </div>
+                        <div class="form-group">
+                            <label for="Email">Email</label>
+                            <input type="Email" name="Email" class="form-control" id="Email"
+                                value="{{ $Mahasiswa->Email }}" aria-describedby="Email">
                         </div>
                         <div class="form-group">
                             <label for="No_Handphone">No_Handphone</label>
