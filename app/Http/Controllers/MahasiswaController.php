66e6a561dc8f0812e1ea1a06fac7b974725cc9ce
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-28 11:42:53 +0700

    Tugas 2 - Memodifikasi MahasiswaController untuk mengatur paginate (menampilkan n data dalam setiap halaman)

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -16,6 +16,7 @@ class MahasiswaController extends Controller
     {
         //fungsi eloquent menampilkan data menggunakan pagination
         $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel
+        $mahasiswas = Mahasiswa::paginate(5);
         $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(6);
         return view('users.index', compact('mahasiswas'))
         -> with('i', (request()->input('page', 1) - 1) * 5);
