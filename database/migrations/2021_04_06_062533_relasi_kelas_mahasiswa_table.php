e7626d1edd86e64d8a3b606855821c1877a00045
Tita Wijayanti <wijayantitita094@gmail.com>
2021-04-06 13:33:28 +0700

    Mengisi file migration yang baru saja dibuat untuk menambahkan foreign key dan menghapus kolom

--- database/migrations/2021_04_06_062533_relasi_kelas_mahasiswa_table.php
+++ database/migrations/2021_04_06_062533_relasi_kelas_mahasiswa_table.php
@@ -13,7 +13,11 @@ class RelasiKelasMahasiswaTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->dropColumn('kelas'); //menghapus kolom kelas
+            $table->unsignedBigInteger('kelas_id')->nullable(); //menambahkan kolom kelas_id
+            $table->foreign('kelas_id')->references('id')->on('kelas'); //menambahkan foreign key di kolom kelas_id
+        });
     }
 
     /**
@@ -23,6 +27,9 @@ class RelasiKelasMahasiswaTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('mahasiswa', function(Blueprint $table) {
+            $table->string('kelas');
+            $table->dropForeign(['kelas_id']);
+        });
     }
 }
