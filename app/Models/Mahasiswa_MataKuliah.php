100925bf2fb5784a2631d99ece4e0b233e243815
Tita Wijayanti <wijayantitita094@gmail.com>
2021-04-10 00:47:48 +0700

    Mengisi fillable pada model mahasiswa_matakuliah

--- app/Models/Mahasiswa_MataKuliah.php
+++ app/Models/Mahasiswa_MataKuliah.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiswa;
 
 class Mahasiswa_MataKuliah extends Model
 {
     use HasFactory;
+    protected $table = 'mahasiswa_matakuliah'; //mendefinisikan model terkait dengan tabel matakuliah
+
+    protected $fillable = [
+        'mahasiswa_id',
+        'matakuliah_id',
+        'nilai'
+    ];
+
 }
