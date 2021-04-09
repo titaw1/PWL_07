38478736cda1cf87f4ed3fc4a90f5c8bd1dadd40
Tita Wijayanti <wijayantitita094@gmail.com>
2021-04-09 22:58:34 +0700

    Melakukan penyesuaian untuk halaman detail pada MahasiswaController

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -83,7 +83,8 @@ class MahasiswaController extends Controller
     public function show($Nim)
     {
         //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
-        // $Mahasiswa = Mahasiswa::find($Nim);
+        //code sebelum dibuat relasi --> $Mahasiswa = Mahasiswa::find($Nim);
+        $Mahasiswa = Mahasiswa::with('kelas')->where('Nim', $Nim)->first();
 
         return view('users.detail', compact('Mahasiswa'));
     }
